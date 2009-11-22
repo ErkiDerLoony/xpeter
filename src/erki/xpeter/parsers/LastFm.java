c6813563fcdf727f1725a3c7a59fe978896b4ec1
Edgar Kalkowski <eMail@edgar-kalkowski.de>
2009-11-22 20:20:28 +0100

    Users can now tell the bot their Last.fm nicknames and the bot automatically uses them for queries.

--- src/erki/xpeter/parsers/LastFm.java
+++ src/erki/xpeter/parsers/LastFm.java
@@ -17,11 +17,20 @@
 
 package erki.xpeter.parsers;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.util.Collection;
+import java.util.TreeMap;
 
 import net.roarsoftware.lastfm.Caller;
 import net.roarsoftware.lastfm.Track;
 import net.roarsoftware.lastfm.User;
+import erki.api.util.Log;
 import erki.api.util.Observer;
 import erki.xpeter.Bot;
 import erki.xpeter.con.Connection;
@@ -30,16 +39,21 @@ import erki.xpeter.msg.TextMessage;
 import erki.xpeter.util.BotApi;
 
 /**
- * Enables the bot to retrieve information from Last.FM and tell people what they are listening to.
+ * Enables the bot to retrieve information from Last.fm and tell people what they are listening to.
  * 
  * @author Edgar Kalkowski
  */
 public class LastFm implements Parser, Observer<TextMessage> {
     
+    private static final String CONFIG_FILE = "config" + File.separator + "Last.fm";
+    
     private static final String LAST_FM_API_KEY = "2ff20341447560ee47656ce6e572107e";
+    
+    private TreeMap<String, String> nicks;
     
     @Override
     public void init(Bot bot) {
+        load();
         bot.register(TextMessage.class, this);
         Caller.getInstance().setUserAgent("xpeter");
     }
@@ -67,7 +81,12 @@ public class LastFm implements Parser, Observer<TextMessage> {
         }
         
         if (text.toLowerCase().trim().equals("np")) {
-            queryNick = msg.getNick();
+            
+            if (nicks.containsKey(msg.getNick())) {
+                queryNick = nicks.get(msg.getNick());
+            } else {
+                queryNick = msg.getNick();
+            }
         }
         
         String match = "[wW]as h(oe|ö)rt (.*?)( gerade)?\\??";
@@ -79,7 +98,12 @@ public class LastFm implements Parser, Observer<TextMessage> {
         match = "[wW]as h(oe|ö)re ich( gerade)?\\??";
         
         if (text.matches(match)) {
-            queryNick = msg.getNick();
+            
+            if (nicks.containsKey(msg.getNick())) {
+                queryNick = nicks.get(msg.getNick());
+            } else {
+                queryNick = msg.getNick();
+            }
         }
         
         if (queryNick != null) {
@@ -97,13 +121,27 @@ public class LastFm implements Parser, Observer<TextMessage> {
                 }
                 
             } else {
-                con.send(new DelayedMessage("Last.FM weiß leider nicht, was " + queryNick
+                con.send(new DelayedMessage("Last.fm weiß leider nicht, was " + queryNick
                         + " gerade hört. :(", 1500));
                 
                 if (((int) (Math.random() * 3)) == 0) {
                     con.send(new DelayedMessage("Tut mir Leid.", 2500));
                 }
             }
+        }
+        
+        match = "([bB]ei )?[lL]ast\\.?[fF][mM] (kennt( man)? mich als|bin ich bekannt "
+                + "(als|unter)) (.*?)";
+        
+        if (text.matches(match)) {
+            String lastFmNick = text.replaceAll(match, "$5");
+            lastFmNick = lastFmNick.replaceAll(" ", "");
+            lastFmNick = lastFmNick.replaceAll("!", "");
+            lastFmNick = lastFmNick.replaceAll("\\.", "");
+            nicks.put(msg.getNick(), lastFmNick);
+            con.send(new DelayedMessage(msg.getNick() + ": Ok, bei Last.fm heißt du also „"
+                    + lastFmNick + "“.", 2000));
+            save();
         }
     }
     
@@ -114,6 +152,44 @@ public class LastFm implements Parser, Observer<TextMessage> {
         } else {
             return "„" + track.getName() + "“ von »" + track.getArtist() + "« auf dem Album „"
                     + track.getAlbum() + "“";
+        }
+    }
+    
+    private void save() {
+        
+        try {
+            ObjectOutputStream fileOut = new ObjectOutputStream(new FileOutputStream(CONFIG_FILE));
+            fileOut.writeObject(nicks);
+            fileOut.close();
+            Log.info("The Last.fm nicks were successfully stored to " + CONFIG_FILE + ".");
+        } catch (FileNotFoundException e) {
+            Log.warning("The Last.fm nicks could not be stored because the config file "
+                    + CONFIG_FILE + " could not be found!");
+        } catch (IOException e) {
+            Log.warning("An error occurred while trying to save the Last.fm nicks ("
+                    + e.getClass().getSimpleName() + ")!");
+        }
+    }
+    
+    @SuppressWarnings("unchecked")
+    private void load() {
+        
+        try {
+            ObjectInputStream fileIn = new ObjectInputStream(new FileInputStream(CONFIG_FILE));
+            nicks = (TreeMap<String, String>) fileIn.readObject();
+            fileIn.close();
+            Log.info("The Last.fm nicks were successfully loaded from " + CONFIG_FILE + ".");
+        } catch (FileNotFoundException e) {
+            Log.info("No saved Last.fm nicks found. Starting with empty mapping.");
+            nicks = new TreeMap<String, String>();
+        } catch (IOException e) {
+            Log.warning("An error occurred while reading the stored Last.fm nicks ("
+                    + e.getClass().getSimpleName() + ").");
+            Log.info("Trying to continue with an empty mapping.");
+        } catch (ClassNotFoundException e) {
+            Log.warning("Could not load Last.fm nicks because the class that stores the nick "
+                    + "mapping could not be found!");
+            Log.info("Trying to continue with an empty mapping.");
         }
     }
 }
