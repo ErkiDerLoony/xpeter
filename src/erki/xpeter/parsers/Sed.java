4243f34c65191c1f3c6401f163a06831002e0a22
Edgar Kalkowski <eMail@edgar-kalkowski.de>
2010-01-10 17:52:39 +0100

    Little improvements for Sed: No need for the final / but only accept non-word characters as delimiters. This way the handling is easier and the bot also does not accidentally react to too many normal sentences.

--- src/erki/xpeter/parsers/Sed.java
+++ src/erki/xpeter/parsers/Sed.java
@@ -28,16 +28,34 @@ public class Sed implements Parser, Observer<TextMessage> {
         String nick = msg.getNick();
         
         if (lastSaid.containsKey(nick) && text.startsWith("s") && text.length() > 2) {
+            String regex = null, replacement = null;
             String delimiter = text.substring(1, 2);
-            String rest = text.substring(2, text.length() - 1);
             
-            if (text.endsWith(delimiter) && rest.contains(delimiter)) {
-                String regex = rest.substring(0, rest.indexOf(delimiter));
-                String replacement = rest.substring(rest.indexOf(delimiter) + 1);
-                Log.debug("Replacing " + regex + " with " + replacement + ".");
-                String result = lastSaid.get(nick).replaceAll(regex, replacement);
-                msg.respond(new Message(nick + " meinte: " + result));
-                return;
+            /*
+             * Only accept non-word characters as delimiters because the bot otherwise reacts to too
+             * many normal sentences.
+             */
+            if (!delimiter.matches("[A-Za-z0-9]")) {
+                
+                String rest = text.substring(2, text.length() - 1);
+                String longrest = text.substring(2);
+                
+                if (text.endsWith(delimiter) && rest.contains(delimiter)) {
+                    regex = rest.substring(0, rest.indexOf(delimiter));
+                    replacement = rest.substring(rest.indexOf(delimiter) + 1);
+                }
+                
+                if (longrest.contains(delimiter)) {
+                    regex = longrest.substring(0, longrest.indexOf(delimiter));
+                    replacement = longrest.substring(longrest.indexOf(delimiter) + 1);
+                }
+                
+                if (regex != null && replacement != null) {
+                    Log.debug("Replacing " + regex + " with " + replacement + ".");
+                    String result = lastSaid.get(nick).replaceAll(regex, replacement);
+                    msg.respond(new Message(nick + " meinte: " + result));
+                    return;
+                }
             }
         }
         
