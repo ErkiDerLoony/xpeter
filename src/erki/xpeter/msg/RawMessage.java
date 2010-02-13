753f060758d6b75dab21ca72b436a8c35ee8f7e1
Edgar Kalkowski <eMail@edgar-kalkowski.de>
2010-02-13 17:08:05 +0100

    Added some missing license headers and updated the others.

--- src/erki/xpeter/msg/RawMessage.java
+++ src/erki/xpeter/msg/RawMessage.java
@@ -1,3 +1,20 @@
+/*
+ * © Copyright 2008–2010 by Edgar Kalkowski <eMail@edgar-kalkowski.de>
+ * 
+ * This file is part of the chatbot xpeter.
+ * 
+ * The chatbot xpeter is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <http://www.gnu.org/licenses/>.
+ */
+
 package erki.xpeter.msg;
 
 /**
--- src/erki/xpeter/util/StorageKey.java
+++ src/erki/xpeter/util/StorageKey.java
@@ -1,11 +1,11 @@
 /*
  * © Copyright 2008–2010 by Edgar Kalkowski <eMail@edgar-kalkowski.de>
  * 
- * This file is part of the chatbot ABCPeter.
+ * This file is part of the chatbot xpeter.
  * 
- * The chatbot ABCPeter is free software; you can redistribute it and/or modify it under the terms
- * of the GNU General Public License as published by the Free Software Foundation; either version 3
- * of the License, or (at your option) any later version.
+ * The chatbot xpeter is free software; you can redistribute it and/or modify it under the terms of
+ * the GNU General Public License as published by the Free Software Foundation; either version 3 of
+ * the License, or (at your option) any later version.
  * 
  * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
  * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
