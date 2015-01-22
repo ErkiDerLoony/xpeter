d28e25d113517b8f75b33c328b2dae985da2a94f
Janosch (Uni) <janosch.henze@gmail.com>
2015-01-22 13:10:58 +0100

    added the possibility to change the dagstuhl date!

--- src/erki/xpeter/parsers/DagstuhlParser.java
+++ src/erki/xpeter/parsers/DagstuhlParser.java
@@ -2,6 +2,8 @@ package erki.xpeter.parsers;
 
 import java.util.Calendar;
 import java.util.Random;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import erki.api.util.Observer;
 import erki.xpeter.Bot;
@@ -35,18 +37,28 @@ public final class DagstuhlParser implements Parser, Observer<TextMessage> {
         "Vorträge halten und hören.",
         "neue Forschungsergebnisse, Erkenntnisse und (Achtung Geek-Witz) Blötzinn vorstellen."
         };
+    private static final String ARE_YOU_SURE_BEGIN = "Bist du dir sicher das Dagstuhl in ";
+    private static final String ARE_YOU_SURE_END = " Tagen ist?";
+    private static final String NEW_DAGSTUHL_DATE_SET = "Danke, dass werde ich so schnell nicht wieder vergessen.";
+    
     
     private Random random;
     
     private long lastReminded;
     
     private long nextDagstuhl;
+    private long possibleNextDagstuhl;
+
+	private boolean askedToSetNewDagstuhlDate;
     
     @Override
     public void init(Bot bot) {
         bot.register(TextMessage.class, this);
         this.random = new Random(31337);
         this.lastReminded = 0l;
+        
+        this.askedToSetNewDagstuhlDate = false;
+        this.possibleNextDagstuhl = 0l;
         
         final Calendar c = Calendar.getInstance();
         c.set(Calendar.DAY_OF_MONTH, 9);
@@ -78,6 +90,44 @@ public final class DagstuhlParser implements Parser, Observer<TextMessage> {
                 
                 msg.respond(new DelayedMessage(message.toString(), 1000));
             }
+        }
+        if (msg.getText().matches("das nächste Mal Dagstuhl ist am \\d{1,2}\\.\\d{1,2}\\.\\d{4}")) {
+        	
+        	// get the date from the message
+        	Pattern pattern = Pattern.compile("\\d{1,4}");
+        	Matcher matcher = pattern.matcher(msg.getText());
+        	
+        	int i=0;
+        	final Calendar c = Calendar.getInstance();
+        	while(matcher.find()) {
+        		String group = matcher.group();
+        		if(i ==0) c.set(Calendar.DAY_OF_MONTH,new Integer(group));
+        		else if( i== 1) c.set(Calendar.MONTH, new Integer(group));
+        		else if (i==2) c.set(Calendar.YEAR, new Integer(group));
+        	}
+        	
+        	//construct response message
+        	
+        	
+        	this.possibleNextDagstuhl = c.getTimeInMillis();
+        	final long days = (this.possibleNextDagstuhl - now) / 1000 / 60 / 60 / 24;
+        	
+        	StringBuilder message = new StringBuilder(ARE_YOU_SURE_BEGIN);
+        	message.append(days);
+        	message.append(ARE_YOU_SURE_END);
+        	
+        	msg.respond(new DelayedMessage(message.toString(), 1000));
+        	this.askedToSetNewDagstuhlDate = true;
+        	
+        }
+        
+        if (this.askedToSetNewDagstuhlDate && msg.getText().equals("Ja, ich bin mir sicher!")) {
+        	this.nextDagstuhl = this.possibleNextDagstuhl;
+        	this.possibleNextDagstuhl = 0l;
+        	this.askedToSetNewDagstuhlDate = false;
+        	
+        	msg.respond(new DelayedMessage(NEW_DAGSTUHL_DATE_SET, 1000));
+        	
         }
     }
 }
